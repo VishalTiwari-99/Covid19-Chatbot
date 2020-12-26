092c993e6a92e490a70abb30aba5aba600a4f88e
hetshah99 <42943424+hetshah99@users.noreply.github.com>
2020-12-26 09:23:14 +0530

    Increased Accuracy (96 percent)

--- train_chatbot.py
+++ train_chatbot.py
@@ -91,7 +91,7 @@ sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
 #fitting and saving the model
-hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=350, batch_size=5, verbose=1)
 model.save('chatbot_model.h5', hist)
 
 print("model created")
